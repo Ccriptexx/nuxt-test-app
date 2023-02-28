94238b0edae3138f3a1d97583127c217b2048ea5
CCriptexx <ccriptexx@gmail.com>
2023-02-28 15:05:44 +0300

    first commit

---
+++ layouts/empty.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="empty-leyout">
+    <nuxt />
+  </div>
+</template>
+
+<style scoped>
+  .empty-leyout{
+    display: flex;
+    justify-content: center;
+    padding-top: 5rem;
+  }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga harum ipsum itaque, iusto modi nostrum tenetur. Ad, aliquid consectetur cum doloribus eos harum necessitatibus nobis placeat quia quis repudiandae rerum tenetur voluptates. Asperiores aspernatur, aut consectetur, consequatur consequuntur dolorem dolorum esse in nemo numquam odio placeat rem, unde? Pariatur!
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet autem distinctio dolores illo inventore molestiae obcaecati repellat saepe voluptate?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dolores eos, ipsum iure praesentium quos.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
